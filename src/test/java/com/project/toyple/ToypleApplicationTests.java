786f44e4475239e1b8fd9391a7b44f1d6fa3ced5
Dayeon Park <ddggs@naver.com>
2022-12-29 16:34:28 +0900

    feat: 개발 환경 설정

---
+++ src/test/java/com/project/toyple/ToypleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.toyple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToypleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
