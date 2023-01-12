324d0481d0f991bbc5c1f80d93f959ff8fe127b2
Sunyoung Park <qkrtjs9321@gmail.com>
2023-01-12 17:11:18 +0900

    feat:새글페이지업데이트

--- src/main/webapp/WEB-INF/views/new/new.js
+++ src/main/webapp/WEB-INF/views/new/new.js
@@ -1,13 +1,18 @@
 function newproject() {
     let proj_name = document.getElementById('proj_name')
+    let response_area = document.getElementById('response_area')
     if(proj_name.value == "") {
         proj_name.focus();
+
+        // input area 진동
         proj_name.classList.add("vibration");
         setTimeout(function() {
             proj_name.classList.remove("vibration");
         }, 400);
+
         proj_name.style.borderColor = 'rgb(225,10,10)'
-        // alert('프로젝트 제목은 필수항목입니다.')
+        // 안내문
+        response_area.style.visibility = 'visible'
     } else {
         if( confirm('프로젝트 이름 : ' + proj_name.value + '\n글을 등록하시겠습니까?')) {
             alert('추가되었습니다.')
@@ -16,7 +21,7 @@ function newproject() {
             alert('취소되었습니다.')
         }
     }
-    
+    return
 }
 function buttonover(obj) {
     obj.style.backgroundColor = "rgb(180, 150, 110)"
