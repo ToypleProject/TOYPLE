593dbc37cbff96f7d7b2f94dddbdfe014f44614e
Yongsoojin <soojin_0503@naver.com>
2023-01-15 19:24:57 +0900

    feat : 로그인 폼 DB 아이디와 비번 대조 여부를 제외하고 완성

--- src/main/webapp/WEB-INF/views/login/login.js
+++ src/main/webapp/WEB-INF/views/login/login.js
@@ -5,22 +5,36 @@ function button_click() {
     let pwd = document.getElementById('pswd1').value;
     let msg1 = document.getElementById('Msg1');
     let msg2 = document.getElementById('Msg2');
-    // console.log(id);
-    // console.log(pwd);
 
 
     // DB와 확인하여 맞는지 여부 체크해야 함
-    if(id == null && pwd == null) {
+    if(id == '' && pwd == '') {
+        // console.log('둘다 null');
         msg1.style.display = 'block';
         msg2.style.display = 'block';
-    } else if(id != null && pwd == null) {
+    } else if(id != '' && pwd == '') {
+        // console.log('pwd가 null');
         msg1.style.display = 'none';
         msg2.style.display = 'block';
-    } else if(id == null && pwd != null) {
+    } else if(id == '' && pwd != '') {
+        // console.log('id가 null');
         msg1.style.display = 'block';
-        msg2.style.display = 'null';
+        msg2.style.display = 'none';
     } else {
+        // console.log('else');
         msg1.style.display = 'none';
         msg2.style.display = 'none';
+        checkting();
     }
+}
+
+function checkting() {
+    //DB와 입력된 id & password 대조
+    let id = document.getElementById('id').value;
+    let pwd = document.getElementById('pswd1').value;
+    console.log(id);
+    console.log(pwd);
+
+    // 아이디 또는 비밀번호가 일치하지 않을 경우
+    swal('아이디 또는 비밀번호를 다시 한번 확인해 주세요.');
 }
