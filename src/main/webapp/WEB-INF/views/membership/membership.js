d3df7f33458879be2e2e3dbafa996b8f12baf7a8
Yongsoojin <soojin_0503@naver.com>
2023-01-16 14:30:12 +0900

    feat : 회원가입 페이지 중복여부체크와 이름칸 등 수정사항구현완료

---
+++ src/main/webapp/WEB-INF/views/membership/membership.js
@@ -0,0 +1,117 @@
+// 비밀번호와 비밀번호 확인이 일치하는지 확인
+function checking() {
+    console.log('checking start');
+    let check_pw = document.getElementById('pswd2').value;
+    let pw = document.getElementById('pswd1').value;
+    let check_comment = document.getElementById('pswd2Msg');
+    // console.log('check_pw : ' + check_pw);
+    // console.log('pw : ' + pw);
+    if(check_pw != pw) {
+        check_comment.value = "0";
+        check_comment.style.display='block';
+    } else {
+        check_comment.value = "1";
+        check_comment.style.display='none';
+    }
+}
+
+function input_ing() {
+    let check_comment = document.getElementById('pswd2Msg');
+    check_comment.style.display = 'none';
+}
+
+// 아이디와 비번, 이메일을 입력하지 않고 가입버튼을 눌렀을 때 '필수 입력란입니다.' 출력
+function membership() {
+    while(true) {
+        // 필수 입력란이 비었는지 확인
+        let checkvalue = 0;
+        for(i = 0; i<array.length; i++) {
+            if(outvalue[i].value != "0") {
+                outvalue[i].style.display = 'block';
+            } else {
+                checkvalue += 1
+                outvalue[i].style.display = 'none';
+            }
+        }
+        if(checkvalue == (array.length)) {
+            // 비밀번호 확인을 했는지 확인
+            if(check_comment.value =="1") {
+                alert('회원가입 완료');
+            } else {
+                // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@회원을 생성해야 함@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                swal('비밀번호 확인 부탁드립니다.');
+            }
+        } else {
+            swal('필수 입력칸을 모두 채워주세요.');
+        }
+        break;
+    }
+}
+
+// 아이디 중복여부 검사
+function id_repeat() {
+    // 임시 변수 id_sample >> DB와 연동해서 값 가져오기??
+    let id_sample = "aa";
+    let id = document.getElementById('id').value;
+    let msg1_2 = document.getElementById('Msg1_2');
+
+    if(id_sample != id) {
+        // console.log(id + sample);
+        msg1_2.style.display = 'none';
+        swal('사용 가능한 아이디 입니다.');
+    } else if(id != '') {
+        msg1_2.style.display = 'block';
+        swal('사용 중인 아이디 입니다.');
+    }
+}
+
+// 이메일 중복여부 검사
+function mail_repeat() {
+        // 임시 변수 m_sample >> DB와 연동해서 값 가져오기??
+    let m_sample = "soojin_0503@naver.com";
+    let mail = document.getElementById('email').value;
+    let msg4_2 = document.getElementById('Msg4_2');
+
+    if(m_sample == mail) {
+        swal('가입 이력이 있는 이메일입니다.');
+        msg4_2.style.display = 'block';
+    } else if(mail != '') {
+        msg4_2.style.display = 'none';
+    }
+}
+
+
+let id = document.getElementById('id').value;
+let pwd = document.getElementById('pswd1').value;
+let name = document.getElementById('name').value;
+let mail = document.getElementById('email').value;
+let check_comment = document.getElementById('pswd2Msg');
+let msg1 = document.getElementById('Msg1');
+let msg2 = document.getElementById('Msg2');
+let msg3 = document.getElementById('Msg3');
+let msg4 = document.getElementById('Msg4');
+
+// 오류가 왜 날까....??????
+// 근데 작동에는 문제가 없음.......뭐지????
+//name을 나는 let으로 선언했는데 const가 됨...왜????
+let array = [id, pwd, mail, name];
+let outvalue = [msg1, msg2, msg3, msg4];
+
+
+// 비밀번호 확인란을 제외하고 focus 되면 바로 하단에 필수 입력이라는 것을 표시해준다.
+function isnullcheck(obj, str) {
+    let num = parseInt(str);
+    console.log(obj.value);
+    if(obj.value == '') {
+        outvalue[num].value = "1";
+        outvalue[num].style.display = 'block';
+    }
+}
+
+// 비밀번호 확인란을 제외하고 출력될 수 있는 키보드 자판이 입력되면 필수 입력이라는 글씨가 사라진다.
+function isnotnullcheck(obj, str) {
+    let num = parseInt(str);
+    console.log(obj.value);
+    outvalue[num].value = "0";
+    outvalue[num].style.display = 'none';
+}
