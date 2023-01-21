912ef35a91dbd3380a87d1b370a06367029bd139
Dayeon Park <ddggs@naver.com>
2023-01-21 18:59:10 +0900

    ViewResolver 설정

---
+++ src/main/java/com/project/toyple/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.project.toyple.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("classpath:templates/");
+        bean.setSuffix(".html");
+        return bean;
+    }
+}
--- src/main/java/com/project/toyple/user/UserController.java
+++ src/main/java/com/project/toyple/user/UserController.java
@@ -1,5 +1,34 @@
 package com.project.toyple.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    // 로그인 창으로 이동
+    @RequestMapping(value="/user/login", method=RequestMethod.GET)
+    public String loginForm() {
+        System.out.println("!!!!!!!!!!login()!!!!!!!!!");
+        return "login/index";
+    }
+
+    // 로그인 창에서 로그인 버튼 눌렀을 때
+    // TODO: js에서 경로 여기로 보내는지 확인하기
+    @RequestMapping(value="/user/login", method=RequestMethod.POST)
+    public String login(String id, String password) {
+        System.out.println("@@@@@@@@@@@@login()@@@@@@@");
+        UserDto userDto = new UserDto();
+        userDto.setUserId(id);
+        userDto.setPassword(password);
+        userService.login(userDto);
+        System.out.println("#######login()#########");
+
+        return "login/index";  // TODO: 메인페이지 완성되면 메인페이지로 바꾸기
+    }
 
 }
