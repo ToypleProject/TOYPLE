95c53642d7c56ed47b0a1c56025ce2768a8d7bb7
Sunyoung Park <qkrtjs9321@gmail.com>
2023-01-24 02:48:05 +0900

    feat:새글추가&메인페이지

--- src/main/webapp/WEB-INF/views/main/main.js
+++ src/main/webapp/WEB-INF/views/main/main.js
@@ -66,42 +66,48 @@ function sort() {
     }
 }
 function resize() {
-    // let offers = document.getElementById('offers')
-    // let offering_table = document.getElementById('offering_table')
-    // console.log(offers)
-    // if (window.innerWidth >= 1200 ){
-    //     offering_table.innerHTML = '<table id="offering_table">'
-    //     for(let i = 0;i<4;i++){
-    //         offering_table.innerHTML += '<tr>'
-    //         for (let j =0;j<offering.length+1;j++){
-    //             offering_table.innerHTML += '<td class="offer"></td>'
-    //         }
-    //         offering_table.innerHTML += '</tr>'
-    //     }
-    //     offering_table.innerHTML += '</table>'
-        
-    // } else if (window.innerWidth >= 900 ){
-    //     offering_table.innerHTML = '<table id="offering_table">'
-    //     for(let i = 0;i<3;i++){
-    //         offering_table.innerHTML += '<tr>'
-    //         for (let j =0;j<offering.length+1;j++){
-    //             offering_table.innerHTML += '<td class="offer"></td>'
-    //         }
-    //         offering_table.innerHTML += '</tr>'
-    //     }
-    //     offering_table.innerHTML += '</table>'
-        
-    // } else if (window.innerWidth >= 600 ){
-    //     offering_table.innerHTML = '<table id="offering_table">'
-    //     for(let i = 0;i<2;i++){
-    //         offering_table.innerHTML += '<tr>'
-    //         for (let j =0;j<offering.length+1;j++){
-    //             offering_table.innerHTML += '<td class="offer"></td>'
-    //         }
-    //         offering_table.innerHTML += '</tr>'
-    //     }
-    //     offering_table.innerHTML += '</table>'
-        
-    // }
-    // sort()
+    let offers = document.getElementById('offers')
+    console.log(offers)
+    if (window.innerWidth >= 1200 ){
+        offering_table.innerHTML = '<table id="offering_table">'
+        let a = offering.length/4
+        for(let j = 0;j<a;j++){
+            const newTr = document.createElement('tr');
+                offering_table.appendChild(newTr);
+            for(let i = 0;i<4;i++){
+                const newTd = document.createElement('td')
+                newTr.appendChild(newTd)
+                newTd.classList.add('offer')
+            }
+            sort();
+        } 
+    } else if (window.innerWidth >= 900 ){
+        offering_table.innerHTML = '<table id="offering_table">'
+        let a = offering.length/3
+            
+        for(let j = 0;j<a;j++){
+            const newTr = document.createElement('tr');
+                offering_table.appendChild(newTr);
+            for(let i = 0;i<3;i++){
+                const newTd = document.createElement('td')
+                newTr.appendChild(newTd)
+                newTd.classList.add('offer')
+            }
+            sort();
+        } 
+    } else if (window.innerWidth >= 600 ){
+        offering_table.innerHTML = '<table id="offering_table">'
+        let a = offering.length/2
+            
+        for(let j = 0;j<a;j++){
+            const newTr = document.createElement('tr');
+                offering_table.appendChild(newTr);
+            for(let i = 0;i<2;i++){
+                const newTd = document.createElement('td')
+                newTr.appendChild(newTd)
+                newTd.classList.add('offer')
+            }
+            sort();
+        } 
+    }
 }
