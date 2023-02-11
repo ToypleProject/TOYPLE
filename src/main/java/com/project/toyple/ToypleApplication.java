6da7cc9394a182f39779471ee6430b4625bd93a0
Dayeon Park <ddggs@naver.com>
2023-02-11 23:18:34 +0900

    feat: application.properties 암호화

--- src/main/java/com/project/toyple/ToypleApplication.java
+++ src/main/java/com/project/toyple/ToypleApplication.java
@@ -1,5 +1,6 @@
 package com.project.toyple;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/project/toyple/config/JasyptConfig.java
@@ -0,0 +1,27 @@
+package com.project.toyple.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    private String password = System.getenv("JASYPT_PASSWORD");
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWITHMD5ANDDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
