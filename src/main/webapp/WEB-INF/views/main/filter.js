02f24175045024aa6d8c50cb5b8ef28140b23639
Sunyoung Park <qkrtjs9321@gmail.com>
2023-02-27 16:43:31 +0900

    feat:메인페이지지역추가,회원가입버튼추가

--- src/main/webapp/WEB-INF/views/main/filter.js
+++ src/main/webapp/WEB-INF/views/main/filter.js
@@ -1,13 +1,25 @@
+function placeFltr(obj) {
+    let label_place = document.getElementsByClassName(obj.classList)
+
+    if(label_place[0].classList.contains('place_selected')){
+        label_place[0].classList.remove('place_selected')
+        console.log(selected_list[0].parentNode)
+    }else {
+        label_place[0].classList.add('place_selected')
+    }
+    showreset('place')
+}
+
 function workFltr(obj) {
     console.log(obj.id)
     let filter_work = document.getElementsByClassName('filter_work')
     for(let i = 0; i<filter_work.length;i++){
         filter_work[i].style.borderBottom=''
     }
-    obj.style.borderBottom='3px solid tan'
+    obj.style.borderBottom='3px solid tan' 
     let lang_filter = document.getElementById('lang_filter')
     if (obj.id == 'lang_fornt_end'){
-        lang_filter.innerHTML = '<label class="langs front_lang f_lang1"><input class="front_lang f_lang1" id="JavaScript" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="JavaScript">JavaScript</label><label class="langs front_lang f_lang2"><input class="front_lang f_lang2" id="TypeScript" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="TypeScript">TypeScript</label><label class="langs front_lang f_lang3"><input class="front_lang f_lang3" id="React" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="React">React</label><label class="langs front_lang f_lang4"><input class="front_lang f_lang4" id="Vue" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Vue">Vue</label><label class="langs front_lang f_lang5"><input class="front_lang f_lang5" id="Svelte" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Svelte">Svelte</label><label class="langs front_lang f_lang6"><input class="front_lang f_lang6" id="Nextjs" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Nextjs">Nextjs</label>'
+        lang_filter.innerHTML = '<div class="langs front_lang f_lang1" onclick="add_lang(this) " onclick="add_lang(this)"><div class="front_lang f_lang1" id="JavaScript" name="slt_lang">JavaScript</div></div><div class="langs front_lang f_lang2 " onclick="add_lang(this)"><div class="front_lang f_lang2" id="TypeScript" name="slt_lang">TypeScript</div></div><div class="langs front_lang f_lang3 " onclick="add_lang(this)"><div class="front_lang f_lang3" id="React" name="slt_lang">React</div></div><div class="langs front_lang f_lang4 " onclick="add_lang(this)"><div class="front_lang f_lang4" id="Vue" name="slt_lang">Vue</div></div><div class="langs front_lang f_lang5 " onclick="add_lang(this)"><div class="front_lang f_lang5" id="Svelte" name="slt_lang">Svelte</div></div><div class="langs front_lang f_lang6 " onclick="add_lang(this)"><div class="front_lang f_lang6" id="Nextjs" name="slt_lang">Nextjs</div></div>'
         let langs = document.getElementsByClassName('langs front_lang')
         let lang_list = document.getElementsByClassName('lang_selected selected_list')
         for(let i = 0; i<langs.length;i++){
@@ -19,7 +31,7 @@ function workFltr(obj) {
             }
         }
     }else if (obj.id == 'lang_back_end') {
-        lang_filter.innerHTML = '<label class="langs back_lang b_lang1"><input class="back_lang b_lang1" id="Java" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Java">Java</label><label class="langs back_lang b_lang2"><input class="back_lang b_lang2" id="Spring" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Spring">Spring</label><label class="langs back_lang b_lang3"><input class="back_lang b_lang3" id="Nodejs" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Nodejs">Nodejs</label><label class="langs back_lang b_lang4"><input class="back_lang b_lang4" id="Nestjs" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Nestjs">Nestjs</label><label class="langs back_lang b_lang5"><input class="back_lang b_lang5" id="Go" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Go">Go</label><label class="langs back_lang b_lang6"><input class="back_lang b_lang6" id="Kotlin" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Kotlin">Kotlin</label><label class="langs back_lang b_lang7"><input class="back_lang b_lang7" id="Express" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Express">Express</label><label class="langs back_lang b_lang8"><input class="back_lang b_lang8" id="MySQL" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="MySQL">MySQL</label><label class="langs back_lang b_lang9"><input class="back_lang b_lang9" id="MongoDB" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="MongoDB">MongoDB</label><label class="langs back_lang b_lang10"><input class="back_lang b_lang10" id="Python" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Python">Python</label><label class="langs back_lang b_lang11"><input class="back_lang b_lang11" id="Django" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Django">Django</label><label class="langs back_lang b_lang12"><input class="back_lang b_lang12" id="php" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="php">php</label><label class="langs back_lang b_lang13"><input class="back_lang b_lang13" id="GraphQL" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="GraphQL">GraphQL</label><label class="langs back_lang b_lang14"><input class="back_lang b_lang14" id="Firebase" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Firebase">Firebase</label>' 
+        lang_filter.innerHTML = '<div class="langs back_lang b_lang1 " onclick="add_lang(this)"><div class="back_lang b_lang1" id="Java" name="slt_lang" value="Java">Java</div></div><div class="langs back_lang b_lang2 " onclick="add_lang(this)"><div class="back_lang b_lang2" id="Spring" name="slt_lang" value="Spring">Spring</div></div><div class="langs back_lang b_lang3 " onclick="add_lang(this)"><div class="back_lang b_lang3" id="Nodejs" name="slt_lang" value="Nodejs">Nodejs</div></div><div class="langs back_lang b_lang4 " onclick="add_lang(this)"><div class="back_lang b_lang4" id="Nestjs" name="slt_lang" value="Nestjs">Nestjs</div></div><div class="langs back_lang b_lang5 " onclick="add_lang(this)"><div class="back_lang b_lang5" id="Go" name="slt_lang" value="Go">Go</div></div><div class="langs back_lang b_lang6 " onclick="add_lang(this)"><div class="back_lang b_lang6" id="Kotlin" name="slt_lang" value="Kotlin">Kotlin</div></div><div class="langs back_lang b_lang7 " onclick="add_lang(this)"><div class="back_lang b_lang7" id="Express" name="slt_lang" value="Express">Express</div></div><div class="langs back_lang b_lang8 " onclick="add_lang(this)"><div class="back_lang b_lang8" id="MySQL" name="slt_lang" value="MySQL">MySQL</div></div><div class="langs back_lang b_lang9 " onclick="add_lang(this)"><div class="back_lang b_lang9" id="MongoDB" name="slt_lang" value="MongoDB">MongoDB</div></div><div class="langs back_lang b_lang10 " onclick="add_lang(this)"><div class="back_lang b_lang10" id="Python" name="slt_lang" value="Python">Python</div></div><div class="langs back_lang b_lang11 " onclick="add_lang(this)"><div class="back_lang b_lang11" id="Django" name="slt_lang" value="Django">Django</div></div><div class="langs back_lang b_lang12 " onclick="add_lang(this)"><div class="back_lang b_lang12" id="php" name="slt_lang" value="php">php</div></div><div class="langs back_lang b_lang13 " onclick="add_lang(this)"><div class="back_lang b_lang13" id="GraphQL" name="slt_lang" value="GraphQL">GraphQL</div></div><div class="langs back_lang b_lang14 " onclick="add_lang(this)"><div class="back_lang b_lang14" id="Firebase" name="slt_lang" value="Firebase">Firebase</div></div>' 
         let langs = document.getElementsByClassName('langs back_lang')
         let lang_list = document.getElementsByClassName('lang_selected selected_list')
         for(let i = 0; i<langs.length;i++){
@@ -31,7 +43,7 @@ function workFltr(obj) {
             }
         }
     }else if (obj.id == 'lang_mobile'){
-        lang_filter.innerHTML = '<label class="langs mob_lang m_lang1"><input class="mob_lang m_lang1" id="Flutter" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Flutter">Flutter</label><label class="langs mob_lang m_lang2"><input class="mob_lang m_lang2" id="Swift" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Swift">Swift</label><label class="langs back_lang b_lang6"><input class="back_lang b_lang6" id="Kotlin" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Kotlin">Kotlin</label><label class="langs mob_lang m_lang4"><input class="mob_lang m_lang4" id="ReactNative" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="ReactNative">ReactNative</label><label class="langs mob_lang m_lang5"><input class="mob_lang m_lang5" id="Unity" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Unity">Unity</label>'
+        lang_filter.innerHTML = '<div class="langs mob_lang m_lang1 " onclick="add_lang(this)"><div class="mob_lang m_lang1" id="Flutter" name="slt_lang" value="Flutter">Flutter</div></div><div class="langs mob_lang m_lang2 " onclick="add_lang(this)"><div class="mob_lang m_lang2" id="Swift" name="slt_lang" value="Swift">Swift</div></div><div class="langs back_lang b_lang6 " onclick="add_lang(this)"><div class="back_lang b_lang6" id="Kotlin" name="slt_lang" value="Kotlin">Kotlin</div></div><div class="langs mob_lang m_lang4 " onclick="add_lang(this)"><div class="mob_lang m_lang4" id="ReactNative" name="slt_lang" value="ReactNative">ReactNative</div></div><div class="langs mob_lang m_lang5 " onclick="add_lang(this)"><div class="mob_lang m_lang5" id="Unity"     name="slt_lang" value="Unity">Unity</div></div>'
         let langs = document.getElementsByClassName('langs mob_lang')
         let lang_list = document.getElementsByClassName('lang_selected selected_list')
         for(let i = 0; i<langs.length;i++){
@@ -47,7 +59,7 @@ function workFltr(obj) {
             }
         }
     }else if (obj.id == 'lang_etc'){
-        lang_filter.innerHTML = '<label class="langs etc_lang e_lang1"><input class="etc_lang e_lang1" id="AWS" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="AWS">AWS</label><label class="langs etc_lang e_lang2"><input class="etc_lang e_lang2" id="Kubernetes" onclick="add_lang(this)" type="checkbox" value="Kubernetes">Kubernetes</label><label class="langs etc_lang e_lang3"><input class="etc_lang e_lang3" id="Docker" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Docker">Docker</label><label class="langs etc_lang e_lang4"><input class="etc_lang e_lang4" id="Git" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Git">Git</label><label class="langs etc_lang e_lang5"><input class="etc_lang e_lang5" id="Figma" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Figma">Figma</label><label class="langs etc_lang e_lang6"><input class="etc_lang e_lang3" id="Zeplin" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Zeplin">Zeplin</label><label class="langs etc_lang e_lang7"><input class="etc_lang e_lang4" id="Jest" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="Jest">Jest</label><label class="langs etc_lang e_lang8"><input class="etc_lang e_lang5" id="C" onclick="add_lang(this)" type="checkbox" name="slt_lang" value="C">C</label>'
+        lang_filter.innerHTML = '<div class="langs etc_lang e_lang1 " onclick="add_lang(this)"><div class="etc_lang e_lang1" id="AWS" name="slt_lang" value="AWS">AWS</div></div><div class="langs etc_lang e_lang2 " onclick="add_lang(this)"><div class="etc_lang e_lang2" id="Kubernetes" value="Kubernetes">Kubernetes</div></div><div class="langs etc_lang e_lang3 " onclick="add_lang(this)"><div class="etc_lang e_lang3" id="Docker" name="slt_lang" value="Docker">Docker</div></div><div class="langs etc_lang e_lang4 " onclick="add_lang(this)"><div class="etc_lang e_lang4" id="Git" name="slt_lang" value="Git">Git</div></div><div class="langs etc_lang e_lang5 " onclick="add_lang(this)"><div class="etc_lang e_lang5" id="Figma" name="slt_lang" value="Figma">Figma</div></div><div class="langs etc_lang e_lang6 " onclick="add_lang(this)"><div class="etc_lang e_lang3" id="Zeplin" name="slt_lang" value="Zeplin">Zeplin</div></div><div class="langs etc_lang e_lang7 " onclick="add_lang(this)"><div class="etc_lang e_lang4" id="Jest" name="slt_lang" value="Jest">Jest</div></div><div class="langs etc_lang e_lang8 " onclick="add_lang(this)"><div class="etc_lang e_lang5" id="C" name="slt_lang" value="C">C</div></div>'
         let langs = document.getElementsByClassName('langs etc_lang')
         let lang_list = document.getElementsByClassName('lang_selected selected_list')
         for(let i = 0; i<langs.length;i++){
@@ -59,25 +71,24 @@ function workFltr(obj) {
             }
         }
     }
-    showreset()
+    showreset('work')
 }
 
 function add_lang(obj){
-    let label_lang = document.getElementsByClassName(obj.classList)
 
-    if(label_lang[0].classList.contains('lang_selected')){
-        label_lang[0].classList.remove('lang_selected')
+    if(obj.classList.contains('lang_selected')){
+        obj.classList.remove('lang_selected')
         let lang_list = document.getElementById('selected_lang_list')
-        let selected_list = document.getElementsByClassName(obj.id)
+        let selected_list = document.getElementsByClassName(obj.childNodes[0].id)
         console.log(selected_list[0].parentNode)
         lang_list.removeChild(selected_list[0].parentNode)
     }else {
-        label_lang[0].classList.add('lang_selected')
+        obj.classList.add('lang_selected')
         let selected_lang_list = document.getElementById('selected_lang_list')
-        selected_lang_list.innerHTML += `<label class="lang_selected selected_list"><input class="${obj.id}" type="checkbox" name="slt_lang" value="${obj.id}" checked onclick="uncheck(this)">${obj.id}<img class="delete_icon" src="../img/delete.svg"></label>`
+        selected_lang_list.innerHTML += `<label class="lang_selected selected_list"><input class="${obj.childNodes[0].id}" type="checkbox" name="slt_lang" value="${obj.childNodes[0].id}" checked onclick="uncheck(this)">${obj.childNodes[0].id}<img class="delete_icon" src="../img/delete.svg"></label>`
     }
     
-    showreset()
+    showreset('work')
 }
 
 function uncheck(obj){
@@ -85,7 +96,7 @@ function uncheck(obj){
     lang_list.removeChild(obj.parentNode)
     let label_lang = document.getElementById(obj.value)
     label_lang.parentNode.classList.remove('lang_selected')
-    showreset()
+    showreset('work')
 }
 
 function reset_filter() {
@@ -97,15 +108,34 @@ function reset_filter() {
     while(lang_selected.length>0){
         lang_selected[0].classList.remove('lang_selected')
     }
-    showreset()
+    showreset('work')
 }
 
-function showreset() {
-    let lang_list = document.getElementById('selected_lang_list')
-    let filter_reset_area = document.getElementById('filter_reset_area')
-    if(lang_list.childNodes.length > 0){
-        filter_reset_area.style.visibility= 'visible';
-    } else {
-        filter_reset_area.style.visibility='hidden'
+function reset_place_filter() {
+    let place_selected = document.getElementsByClassName('place_selected')
+    while(place_selected.length > 0){
+        place_selected[0].classList.remove('place_selected')
+    }
+    showreset('place')
+}
+
+function showreset(a) {
+    if(a=='work'){
+        let lang_list = document.getElementById('selected_lang_list')
+        let filter_reset_area = document.getElementById('filter_reset_area')
+        if(lang_list.childNodes.length > 0){
+            filter_reset_area.style.visibility= 'visible';
+        } else {
+            filter_reset_area.style.visibility='hidden'
+        }
+    }
+    if(a=='place'){
+        let place_selected = document.getElementsByClassName('place_selected')
+        let filter_place_reset_area = document.getElementById('filter_place_reset_area')
+        if(place_selected.length > 0){
+            filter_place_reset_area.style.visibility='visible'
+        } else {
+            filter_place_reset_area.style.visibility='hidden'
+        }
     }
 }
